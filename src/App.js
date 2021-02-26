174d05df4c9ccdf492325cfc5035d8a16c32e0ff
Pravin <pravin.kumar@appsintegra.com>
2021-02-26 16:12:24 +0400

    task

--- src/App.js
+++ src/App.js
@@ -2,50 +2,64 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 import axios from "axios";
-
-import { Container, InputGroup, Table, Image } from "react-bootstrap";
-import { FaPlus } from "react-icons/fa";
+import {
+  Container,
+  InputGroup,
+  Table,
+  Image,
+  FormControl,
+  Button,
+  Alert
+} from "react-bootstrap";
+import Python from "./images/Python.png";
+import JavaScript from "./images/JavaScript.png";
 
 export default function App() {
   let [responseData, setResponseData] = React.useState("");
+  let [validationStatus, setvalidationStatus] = React.useState("");
   let textInput = React.createRef();
   const fetchData = React.useCallback(() => {
-    const username = textInput.current.value;
-    if (username) {
-      axios({
-        method: "GET",
-        url: `https://api.github.com/users/${username}/gists`,
-        headers: {
-          "content-type": "application/json"
-        }
-      })
-        .then((response) => {
-          console.log(response);
-          setResponseData(response.data);
+    const username = textInput?.current?.value;
+    username
+      ? axios({
+          method: "GET",
+          url: `https://api.github.com/users/${username}/gists`,
+          headers: {
+            "content-type": "application/json"
+          }
         })
-        .catch((error) => {
-          console.log(error);
-        });
-    } else {
-      setResponseData("");
-    }
+          .then((response) => {
+            console.log(response);
+            setResponseData(response.data);
+            setvalidationStatus("");
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+      : setvalidationStatus("1");
   }, []);
 
-  // React.useEffect(() => {
-  //   fetchData();
-  // }, [fetchData]);
   return (
     <Container>
       <div className="App">
         <h2>Get GIT Data</h2>
 
         <InputGroup className="mb-3">
-          <input ref={textInput} placeholder="Type a message..." />
-
-          <button type="button" onClick={fetchData} placeholder="wfng92">
+          <FormControl
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+            ref={textInput}
+          />
+          <Button variant="primary" placeholder="wfng92" onClick={fetchData}>
             Search
-          </button>
+          </Button>
         </InputGroup>
+        {validationStatus ? (
+          <Alert variant="danger">Please provide username</Alert>
+        ) : (
+          ""
+        )}
         {responseData ? (
           <Table striped bordered hover>
             <thead>
@@ -55,7 +69,16 @@ export default function App() {
               </tr>
             </thead>
             <tbody>
-              {responseData.map((data) => {
+              {responseData.map((data, keys) => {
+                const file = () => {
+                  if (
+                    data.files[Object.keys(data.files)].language === "Python"
+                  ) {
+                    return Python;
+                  } else {
+                    return JavaScript;
+                  }
+                };
                 return (
                   <tr>
                     <td>
@@ -66,12 +89,7 @@ export default function App() {
                         "Python" ||
                       data.files[Object.keys(data.files)].language ===
                         "JavaScript" ? (
-                        <Image
-                          src={`${
-                            data.files[Object.keys(data.files)].language
-                          }.png`}
-                          thumbnail
-                        />
+                        <Image src={file()} />
                       ) : (
                         Object.keys(data.files)
                       )}
